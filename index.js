5caf4a9bb7f7410c5a5bd6f74e1125cc1adf96db
Senmiao Liu <zir.echo@gmail.com>
2012-02-21 00:16:55 +0800

    dbRootCollection -> dbCollection

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.createServer = function(options) {
     dbName: 'lazy_image_test',
     dbHost: '127.0.0.1',
     dbPort: 27017,
-    dbRootCollection: 'images'
+    dbCollection: 'images'
   };
   var options_ = genji.extend({}, defaultOptions, options);
   var db = connect(options_.dbHost, options_.dbPort, {poolSize: 10}).db(options_.dbName, {});
@@ -16,7 +16,10 @@ exports.createServer = function(options) {
   genji.use('conditional-get');
 
   var app = require('./app');
-  app.init(db, options_.dbRootCollection);
+  app.init(db, {
+    collection: options_.dbCollection,
+    tmpDir: options_.tmpDir
+  });
 
   // create a http server
   var server = genji.createServer();
