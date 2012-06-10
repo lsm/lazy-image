1eff859085e06715fe4b68203fbdc8124265a5d3
Senmiao Liu <zir.echo@gmail.com>
2012-06-10 22:49:51 +0800

    ImageProcessApp

--- app.js
+++ app.js
@@ -99,8 +99,8 @@ var ImageUploadApp = App('ImageUploadApp', {
     }, blob, imageDoc);
   },
 
-  routes: {
-    uploadImageFile:{method: 'post', url:'/upload/image/file', handleFunction:function (handler) {
+  routes:{
+    uploadImageFile:{method:'post', url:'/upload/image/file', handleFunction:function (handler) {
       var self = this;
       this.app.parseRequest(handler.context.request, function (err, fields, files) {
         if (err) {
@@ -113,7 +113,7 @@ var ImageUploadApp = App('ImageUploadApp', {
           self.app.saveImageFile.call(self, file.path, imageDoc);
         }
       });
-    }, handlerClass: BaseHandler},
+    }, handlerClass:BaseHandler},
     uploadImageBlob:{method:'post', url:'/upload/image/blob', handleFunction:function (handler) {
       var request = handler.context.request;
       var len = Number(request.headers['content-length']);
@@ -139,10 +139,10 @@ var ImageUploadApp = App('ImageUploadApp', {
           self.app.saveImageBlob.call(self, buff, imageDoc);
         });
       }
-    }, handlerClass: BaseHandler}
+    }, handlerClass:BaseHandler}
   },
 
-  routeResults: {
+  routeResults:{
     saveImageFile:function (err, imageDoc) {
       if (err) {
         this.handler.error(500, 'upload failed');
@@ -172,4 +172,175 @@ var ImageUploadApp = App('ImageUploadApp', {
   }
 });
 
+
+var ImageProcessApp = App('ImageProcessApp', {
+
+  init:function (options) {
+    var options_ = extend({}, ImageUploadApp.defaultOptions, options);
+    var db = connect(options_.dbHost, options_.dbPort, {poolSize:options_.dbPoolSize}).db(options_.dbName, {});
+    this.processer = new ImageProcesser(options_, db);
+    db.open()
+      .fail(function (err) {
+        console.trace('Can not connect to mongodb with options: ');
+        console.error(options_);
+        throw err;
+      });
+  },
+
+  getImageDoc:function (query, callback) {
+    var queryDoc;
+    if (typeof query === 'string') {
+      queryDoc = query.length === 40 ? {_id:query} : {filename:query};
+    } else {
+      queryDoc = query;
+    }
+    this.processer.imageCollection
+      .findOne(queryDoc)
+      .then(
+      function (imageDoc) {
+        callback(null, imageDoc);
+      }).fail(callback);
+  },
+
+  getImage:function (filename, options) {
+    var lazyProcess = false;
+    var processer = this.app.processer;
+    var origFilename = filename;
+    if (filename.length === 40 && options.hash && options.width && options.height) {
+      options.id = filename;
+      lazyProcess = options.hash === generateImageHash(options, processer.privateKey);
+      filename = generateImageFilename(filename, options);
+    } else {
+      // requesting original image file
+      if (processer.denyOriginal) {
+        this.emit('getImage', 'Image not found');
+        return;
+      }
+    }
+    var self = this;
+    this.app.getImageDoc(filename, function (err, imageDoc) {
+      if (err) {
+        self.emit('getImage', err);
+      } else if (imageDoc) {
+        imageDoc.data = imageDoc.data.value();
+        self.emit('getImage', null, imageDoc);
+      } else {
+        if (lazyProcess) {
+          // find the original image and resize
+          processer.imageCollection.findOne({_id:origFilename}).and(
+            function (defer, imageDoc) {
+              if (!imageDoc) {
+                self.emit('getImage', 'Image not found');
+              } else {
+                processer.resize(imageDoc, options, defer);
+              }
+            }).then(function (resizedDoc) {
+              self.app.getImageDoc(resizedDoc._id, function (err, imageDoc) {
+                if (err || !imageDoc) {
+                  self.emit('getImage', 'Image not found');
+                } else {
+                  imageDoc.data = imageDoc.data.value();
+                  self.emit('getImage', null, imageDoc);
+                }
+              });
+            })
+            .fail(function (err) {
+              self.emit('getImage', err);
+            });
+        } else {
+          self.emit('getImage', 'Image not found');
+        }
+      }
+    });
+  },
+
+  processImage: function(options) {
+    var url = options.url;
+    var processer = this.app.processer;
+    var deferred = processer.saveImageFromUrl(url);
+    var result = [];
+    var originalDoc;
+    var self = this;
+    deferred.and(function (defer, imageDoc) {
+        delete imageDoc.data;
+        originalDoc = imageDoc;
+        if (options.noLossless) {
+          handler.sendJSON([imageDoc]);
+          self.emit('processImage', null, [imageDoc]);
+          return true;
+        } else {
+          //lossless compress
+          result.push(imageDoc);
+          processer.compress(imageDoc, 100, defer);
+        }
+      },
+      function (defer, compressedDoc) {
+        if (!options.noLossless) {
+          result.push(compressedDoc);
+        }
+        if (options.width && options.height) {
+          processer.resize(originalDoc, options, defer);
+        } else {
+          self.emit('processImage', null, result);
+        }
+      },
+      function (defer, resizedDoc) {
+        result.push(resizedDoc);
+        self.emit('processImage', null, result);
+      })
+      .fail(function (err) {
+        self.emit('processImage', err);
+      });
+  },
+
+  routes:{
+    getImage:{method:'get', url:'/get/([0-9a-zA-Z]{40})\\.(?:jpg|png|gif)'},
+    processImage:{method: 'get', url: '/process'},
+    notFound:{method: 'notFound', url: '^/*', handleFunction:function (handler) {
+      handler.error(404, 'invalid endpoint');
+      console.error('Invalid endpoint: ' + this.request.url);
+    }}
+  },
+
+  routeResults:{
+    getImage:function (err, imageDoc) {
+      if (err || !imageDoc) {
+        this.handler.error(404, 'Image not found');
+        console.error(err.stack || err);
+        return;
+      }
+      this.handler.sendAsFile(imageDoc.data, {type: imageDoc.type});
+    },
+    processImage:function (err, imageDocs) {
+      if (err || !Array.isArray(imageDocs)) {
+        this.handler.error(500, 'Image processing error');
+        console.error(err.stack || err);
+        return;
+      }
+      this.handler.sendJSON(imageDocs);
+    }
+  }
+
+});
+
+
+function generateImageHash(options, key) {
+  if (!options.hasOwnProperty('watermark')) {
+    options.watermark = '0';
+  }
+  return sha1([options.id, options.width, options.height, options.quality, options.watermark, key].join('_'));
+}
+
+function generateImageFilename(id, options) {
+  options.quality = options.quality || 100;
+  var filename = [id, options.quality, options.width + 'x' + options.height];
+  if (options.watermark === '1') {
+    filename.push('watermarked');
+  }
+  return filename.join('_');
+}
+
 exports.ImageUploadApp = ImageUploadApp;
+exports.ImageProcessApp = ImageProcessApp;
+exports.generateImageFilename = generateImageFilename;
+exports.generateImageHash = generateImageHash;
--- example/server.js
+++ example/server.js
@@ -20,10 +20,17 @@ var options = {
   maxFieldsSize:8388608 // 8MB
 };
 
+genji.use('conditional-get');
 
+// upload app
 var uploadApp = new lazyImage.ImageUploadApp(options);
 genji.loadApp(uploadApp);
 
+// process app
+var processApp = new lazyImage.ImageProcessApp(options);
+genji.loadApp(processApp);
+
+// index.html and jquery
 var route = genji.route();
 
 route.get('^/$', function (handler) {
@@ -32,8 +39,13 @@ route.get('^/$', function (handler) {
 
 route.get('^/jquery-1.7.1.min.js', function (handler) {
   handler.staticFile(Path.join(__dirname, './jquery-1.7.1.min.js'));
-})
+});
 
 var server = genji.createServer();
 
 server.listen(7001, '127.0.0.1');
+
+process.on('uncaughtException', function(err) {
+  console.log('LazyImage error:');
+  console.log(err.stack || err);
+});
